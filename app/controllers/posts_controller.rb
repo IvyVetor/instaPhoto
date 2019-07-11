1186c037b7c8c3a9632bca543d78d05f9e13d279
Ivy Vetor <ivyvetor@gmail.com>
2019-07-10 20:06:41 -0400

    reverse order of photos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by { |post| post.updated_at }.reverse!
   end
 
   # GET /posts/1
